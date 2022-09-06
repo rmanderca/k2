903b071170368e1f029fa4934e6e6015465ce947
Ethan Post <post.ethan@gmail.com>
2022-09-06 13:28:26 -0500

    Update app_install.sql

--- app/demo/app_install.sql
+++ app/demo/app_install.sql
@@ -1,11 +1,21 @@
 
-/*
-schema.sql - Contains most of the DDL for you application. 
+-- IMPORTANT: The files below should be idempotent (https://en.wikipedia.org/wiki/Idempotence).
 
-This file should be idempotent! It should always produce the correct
-schema regardless of the state of the schema it is running against.
+/* 
+This section installs your custom application's files (replace 'app' with your app folder name).
 */
 
+-- @../../config/app/secret_arcsql_cfg.sql
+-- @../../config/app/secret_apex_utl2_config.sql 
+-- @../../config/app/secret_k2_config.sql
+-- @../../config/app/secret_saas_auth_config.sql 
+-- @../../config/app/secret_app_config.sql
+-- @app_schema.sql
+-- @app_pkgh.sql
+-- @app_pkgb.sql
+-- @app_stat_in_ins_trg.sql
+-- @app_stat_work_ins_trg.sql 
+-- @app_stat_work_upd_trg.sql 
 
 /*
 saas_auth_events.sql - Override default procedures in SAAS_AUTH with your custom code.
