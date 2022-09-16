3dc43974fe6803fe45bcc77610c7d79f58a1bcfa
Ethan Post <post.ethan@gmail.com>
2022-09-16 13:36:19 +0000

    Delete secret_oracle_email_config.sql

--- config/statzilla/dev/secret_oracle_email_config.sql
+++
@@ -1,9 +0,0 @@
-
-
-begin
-    apex_instance_admin.set_parameter('SMTP_HOST_ADDRESS', 'smtp.email.us-phoenix-1.oci.oraclecloud.com');
-    apex_instance_admin.set_parameter('SMTP_USERNAME', 'ocid1.user.oc1..aaaaaaaaimhjvwgjsdnz6lb7etdqny6aojia2i54x4gohbznt5kvqkg2gnma@ocid1.tenancy.oc1..aaaaaaaa65expmyve24tkt4aqu2iysx4dok7ef3r553mmlg23jew4bgfg44a.ox.com');
-    apex_instance_admin.set_parameter('SMTP_PASSWORD', '3:4IQwWp$Z05AygOCUU(');
-    commit;
-end;
-/
