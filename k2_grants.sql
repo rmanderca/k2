6b62c2b80e9967de1349902b632dbfee2f34af2e
Ethan Post <post.ethan@gmail.com>
2022-09-16 10:34:02 -0500

    Split user and grants.

---
+++ k2_grants.sql
@@ -0,0 +1,5 @@
+define username='x'
+
+@./lib/arcsql/arcsql_user.sql 
+
+@./lib/statzilla/statzilla_grants.sql 
--- k2_user.sql
+++ k2_user.sql
@@ -1,12 +1,12 @@
 define username='x'
---define password='xxxxxxxxxxxxxxxxxxx'
---
---create user &username identified by &password;
---grant create session to &username;
---
----- One of these might work. Tablespace name depends on your env of courses
---alter user &username quota 20g on users;
----- alter user &username quota 20g on data;
+define password='xxxxxxxxxxxxxxxxxxx'
+
+create user &username identified by &password;
+grant create session to &username;
+
+-- One of these might work. Tablespace name depends on your env of courses
+alter user &username quota 20g on users;
+-- alter user &username quota 20g on data;
 
 @./lib/arcsql/arcsql_user.sql 
 
