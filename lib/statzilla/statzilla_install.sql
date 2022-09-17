f23f94353d4085662a2baca5d90169d8833b36ef
Ethan Post <post.ethan@gmail.com>
2022-09-17 10:20:57 -0500

    Not going to manage version for modules in ./lib.

--- lib/statzilla/statzilla_install.sql
+++ lib/statzilla/statzilla_install.sql
@@ -1,9 +1,10 @@
 
+exec drop_package('statzilla_version');
+exec drop_package('statzilla_b');
 @statzilla_patch.sql
 @statzilla_schema.sql
 @statzilla_pkgh.sql
 @statzilla_pkgb.sql
 @statzilla_trg.sql
-@statzilla_version.sql
 
 select 'APP install complete.' MESSAGE from dual;
--- lib/statzilla/statzilla_version.sql
+++
@@ -1,6 +0,0 @@
-
--- uninstall: exec drop_package('statzilla_version');
-create or replace package statzilla_version as 
-    version number := 20220830;
-end;
-/
