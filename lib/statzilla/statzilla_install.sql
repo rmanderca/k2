f42ec21b8a6d879d1c8f9813b861895aa8dc7c9a
Ethan Post <post.ethan@gmail.com>
2022-09-17 16:08:31 -0500

    Rebuild statzilla uninstall.

--- lib/statzilla/statzilla_install.sql
+++ lib/statzilla/statzilla_install.sql
@@ -9,4 +9,4 @@ exec drop_package('statzilla_b');
 @statzilla_get_oracle_metrics.prc
 @app_scheduler_jobs.sql
 
-select 'APP install complete.' message from dual;
+select 'Statzilla install complete.' message from dual;
